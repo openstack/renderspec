bdcf1290774db9487bcfcb46be1c8a412851ec72
Thomas Bechtold <tbechtold@suse.com>
2015-10-05 08:40:44 +0200

    Initial version of renderspec
    Change-Id: I7a0098740107168836e09f69dc5099db38eead62

---
+++ README.rst
@@ -0,0 +1,35 @@
+renderspec
+==========
+
+renderspec is a tool to convert a .spec.j2 Jinja2 template to
+a rpm .spec file which is usable for different distributions
+and follow their policies and processes.
+
+For more information about the OpenStack rpm packaging initiative, please
+follow the link to the wiki page:
+
+   https://wiki.openstack.org/wiki/Rpm-packaging
+
+In the unfortunate event that bugs are discovered, they should
+be reported to the appropriate bug tracker. If you obtained
+the software from a 3rd party operating system vendor, it is
+often wise to use their own bug tracker for reporting problems.
+In all other cases use the master OpenStack bug tracker,
+available at:
+
+   https://bugs.launchpad.net/renderspec
+
+Developers wishing to work on renderspec should always base
+their work on the latest code, available from the master GIT
+repository at:
+
+   https://git.openstack.org/cgit/openstack/renderspec
+
+Developers should also join the discussion on the mailing list,
+at:
+
+   http://lists.openstack.org/cgi-bin/mailman/listinfo/openstack-dev
+
+or join the IRC channel on
+
+   http://webchat.freenode.net/?channels=openstack-rpm-packaging
---
+++ renderspec/__init__.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# Copyright (c) 2015 SUSE Linux GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import argparse
+import os
+import platform
+from jinja2 import Environment, FileSystemLoader
+
+
+###############
+# jinja2 filter
+###############
+def _filter_noop(value):
+    """do nothing filter"""
+    return value
+
+
+def _filter_license_spdx2fedora(value):
+    """convert a given known spdx license to another one"""
+    # more values can be taken from from https://github.com/hughsie/\
+    #    appstream-glib/blob/master/libappstream-builder/asb-package-rpm.c#L76
+    mapping = {
+        "Apache-1.1": "ASL 1.1",
+        "Apache-2.0": "ASL 2.0",
+        "BSD-3-Clause": "BSD",
+        "GPL-1.0+": "GPL+",
+        "GPL-2.0": "GPLv2",
+        "GPL-2.0+": "GPLv2+",
+        "GPL-3.0": "GPLv3",
+        "GPL-3.0+": "GPLv3+",
+        "LGPL-2.1": "LGPLv2.1",
+        "LGPL-2.1+": "LGPLv2+",
+        "LGPL-2.0": "LGPLv2 with exceptions",
+        "LGPL-2.0+": "LGPLv2+ with exceptions",
+        "LGPL-3.0": "LGPLv3",
+        "LGPL-3.0+": "LGPLv3+",
+        "MIT": "MIT with advertising",
+        "MPL-1.0": "MPLv1.0",
+        "MPL-1.1": "MPLv1.1",
+        "MPL-2.0": "MPLv2.0",
+        "Python-2.0": "Python",
+    }
+    return mapping[value]
+
+
+def _filter_pypackname(value):
+    # FIXME(toabctl): use pymod2pkg when it supports SUSE and Fedora
+    return "python-%s" % (value)
+
+
+def generate_spec(spec_style, input_template_path):
+    """generate a spec file with the given style and the given template"""
+    env = Environment(loader=FileSystemLoader(
+        os.path.dirname(input_template_path)))
+
+    # register dist specific filter
+    if spec_style == 'suse':
+        env.filters['license_normalize'] = _filter_noop
+    elif spec_style == 'fedora':
+        env.filters['license_normalize'] = _filter_license_spdx2fedora
+    else:
+        raise Exception("Unknown spec style '%s' given" % (spec_style))
+    # common filters
+    env.filters['pypackname_normalize'] = _filter_pypackname
+
+    template = env.get_template(os.path.basename(input_template_path))
+    return template.render()
+
+
+def _get_default_distro():
+    distname, version, id_ = platform.linux_distribution()
+    if "suse" in distname.lower():
+        return "suse"
+    elif "fedora" in distname.lower():
+        return "fedora"
+    else:
+        return "unknown"
+
+
+def process_args():
+    distro = _get_default_distro()
+    parser = argparse.ArgumentParser(
+        description="Convert a .spec.j2 template into a .spec")
+    parser.add_argument("-o", "--output",
+                        help="output filename instead of stdout")
+    parser.add_argument("--spec-style", help="distro style you want to use. "
+                        "default: %s" % (distro), default=distro,
+                        choices=['suse', 'fedora'])
+    parser.add_argument("input-template",
+                        help="specfile jinja2 template to use")
+    return vars(parser.parse_args())
+
+
+def main():
+    args = process_args()
+    spec = generate_spec(args['spec_style'], args['input-template'])
+    if args['output']:
+        with open(args['output'], "w") as o:
+            o.write(spec)
+    else:
+        print(spec)
+
+
+if __name__ == '__main__':
+    main()
